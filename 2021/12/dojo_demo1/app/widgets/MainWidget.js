ede67c303b0120049a446ddbf79a0a334cc28278
Tomasz Półgrabia <tomasz.polgrabia@protonmail.com>
2021-12-19 20:32:18 +0100

    Rest + https://github.com/dojo/dijit/blob/master/tests/resources/TestContextRequireWidget.js

--- 2021/12/dojo_demo1/app/widgets/MainWidget.js
+++ 2021/12/dojo_demo1/app/widgets/MainWidget.js
@@ -2,8 +2,9 @@ define([
 	"dijit/_TemplatedMixin",
 	"dijit/_WidgetBase",
 	"dojo/_base/declare",
+	"./ChildWidget",
 	"dojo/text!./MainWidget.html"
-], function(_TemplatedMixin, _WidgetBase, declare, template) {
+], function(_TemplatedMixin, _WidgetBase, declare, ChildWidget, template) {
 	return declare("app/widgets/MainWidget", [_WidgetBase, _TemplatedMixin], {
 		templateString: template
 	});
