baa1f2f95e7c686bf57d1bcb6e676eec70e65754
Tomasz Półgrabia <tomasz.polgrabia@protonmail.com>
2022-01-14 22:40:20 +0100

    Fixing struct serialization issue (small caps).

--- 2022/01/golang_demo1/Program.go
+++ 2022/01/golang_demo1/Program.go
@@ -6,16 +6,16 @@ import (
 )
 
 type StandardResponse struct {
-	result int
-	status string
+	Result int
+	Status string
 }
 
 func main() {
 	hf := http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
 		writer.Header().Set("Content-Type", "application/json")
 		m := StandardResponse{
-			result: 200,
-			status: "OK",
+			Result: 200,
+			Status: "OK",
 		}
 
 		p, _ := json.Marshal(m)
