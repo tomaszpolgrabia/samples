17cc53fb1e3a6d0abcc5ea3d651b7b4ec4cd44d1
Tomasz Półgrabia <tomasz.polgrabia@protonmail.com>
2022-01-17 22:36:10 +0100

    Adding go func examples and global .gitignore.

---
+++ 2022/01/go_func1/Program.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mariomac/gostream/stream"
+)
+
+func isEven(n int) bool {
+	return n%2 == 0
+}
+
+func main() {
+	fmt.Println("Hello World")
+	stream.Of(0, 1, 2, 3, 4).
+		Filter(func(n int) {
+			return n%2 == 0
+		}).
+		ForEach(func(n int) {
+			fmt.Printf("Number %v\n", n)
+		})
+}
