d9c5879fbe4f0ae349518f5364644d2dbf7a299b
Tomasz Półgrabia <tomasz.polgrabia@protonmail.com>
2022-01-24 20:39:50 +0100

    Adding first maven mojo

---
+++ 2022/01/mojo_demo1/maven_mojo1/src/main/java/ch/polgrabia/demos/MyMojo.java
@@ -0,0 +1,53 @@
+package ch.polgrabia.demos;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+@Mojo(name = "my-mojo", defaultPhase = LifecyclePhase.COMPILE)
+public class MyMojo
+        extends AbstractMojo {
+    private final Log logger = getLog();
+
+    @Parameter(property = "outputDirectory", defaultValue = "target")
+    private File outputDirectory;
+
+    public void execute()
+            throws MojoExecutionException {
+        logger.info("Starting execution...");
+        File f = outputDirectory;
+
+        if (!f.exists()) {
+            if (!f.mkdirs()) {
+                throw new MojoExecutionException("Failed to create directory: " + f.getAbsolutePath());
+            }
+        }
+
+        File touch = new File(f, "touch.txt");
+
+        FileWriter w = null;
+        try {
+            w = new FileWriter(touch);
+
+            w.write("touch.txt");
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touch, e);
+        } finally {
+            if (w != null) {
+                try {
+                    w.close();
+                } catch (IOException e) {
+                    logger.error("I couldn't close it the file", e);
+                }
+            }
+            logger.info("Ending execution...");
+        }
+    }
+}
